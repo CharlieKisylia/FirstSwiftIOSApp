f4824a0efabed8c23c1c5545ac43ff2188e749bf
Charlie Kisylia <kisyliac@yahoo.com>
2024-08-13 15:02:03 -0700

    Initial Commit

---
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  FirstIOSApp
+//
+//  Created by Charlie Kisylia on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
